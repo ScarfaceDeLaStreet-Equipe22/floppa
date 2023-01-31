79ade7dd31ad7313a463b15c7a1028c6287459ce
bouiz05 <bouizmed2000@gmail.com>
2023-01-31 17:23:15 -0500

    requête PostSeller fonctionnel

--- src/main/java/ulaval/glo2003/Seller/SellerRessource.java
+++ src/main/java/ulaval/glo2003/Seller/SellerRessource.java
@@ -5,14 +5,26 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import ulaval.glo2003.Utils.MissingParamException;
 
 @Path("/sellers")
 public class SellerRessource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public Response seller(SellerRequest sellerRequest){
-        SellerResponse sellerResponse = new SellerResponse(sellerRequest.name, sellerRequest.bio, sellerRequest.birthDate);
-        Seller seller = new Seller(sellerRequest.name, sellerRequest.bio, sellerRequest.birthDate);
+        String name = sellerRequest.name;
+        String bio = sellerRequest.bio;
+        String birthDate = sellerRequest.birthDate;
+
+        //validateSellerRequest(sellerRequest);
+        Seller seller = new Seller(name, bio, birthDate);
+
         return Response.ok(seller).build();
     }
+
+    /*private void validateSellerRequest(SellerRequest sellerRequest){
+        if(sellerRequest.name == null || sellerRequest.bio == null || sellerRequest.birthDate == null){
+            throw new MissingParamException("The name, bio and birthdate should not be blank");
+        }
+    }*/
 }
---
+++ src/main/java/ulaval/glo2003/Utils/MissingParamException.java
@@ -0,0 +1,7 @@
+package ulaval.glo2003.Utils;
+
+public class MissingParamException extends RuntimeException{
+    public MissingParamException(String message) {
+        super(message);
+    }
+}
