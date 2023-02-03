2cd46f4e7beceb29b97f4afadf5ef762683d9903
Sandrine Comeau <sandrine.comeau.1@ulaval.ca>
2023-02-03 18:21:26 -0500

    premier essai obtention vendeur

--- src/main/java/ulaval/glo2003/Seller/Seller.java
+++ src/main/java/ulaval/glo2003/Seller/Seller.java
@@ -1,17 +1,20 @@
 package ulaval.glo2003.Seller;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 public class Seller {
 
     public String name;
     public String bio;
     public LocalDate birthDate;
+    private final String id;
 
     public Seller(String name, String bio, LocalDate birthDate) {
         this.name = name;
         this.bio = bio;
         this.birthDate = birthDate;
+        this.id = UUID.randomUUID().toString();
     }
 
 
@@ -26,6 +29,7 @@ public class Seller {
     public LocalDate getBirthDate() {
         return birthDate;
     }
-
-
+    public String getId() {
+        return id;
+    }
 }
--- src/main/java/ulaval/glo2003/Seller/SellerRessource.java
+++ src/main/java/ulaval/glo2003/Seller/SellerRessource.java
@@ -1,16 +1,24 @@
 package ulaval.glo2003.Seller;
 
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import ulaval.glo2003.Utils.MissingParamException;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Path("/sellers")
 public class SellerRessource {
 
+    private final ArrayList<Object> sellers;
+
     public SellerRessource(){
+        this.sellers = new ArrayList<>();
     }
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
@@ -24,9 +32,33 @@ public class SellerRessource {
         SellerParams sellerParams = new SellerParams(name, bio, birthDate);
         seller = new Seller(sellerParams.name, sellerParams.bio, sellerParams.birthDate);
 
+        sellers.add(seller);
 
         return Response.status(201).entity(seller).build();
     }
+
+    @GET
+    public Response getAllSellers(){
+        List<SellerResponse > sellerResponses = this.sellers
+                .stream()
+                .map(seller -> new SellerResponse(seller.getId(), seller.getName(), seller.getCreatedAt(), seller.getBio()))
+                .collect(Collectors.toList());
+        return Response.ok(sellerResponses).build();
+    }
+
+    @GET
+    @Path("{sellerId}")
+    public Response getSeller(@PathParam("sellerId") String sellerId){
+        Optional<Seller> foundSeller = this.sellers
+                .stream()
+                .filter(seller -> seller.getId().equals(sellerId))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Seller not found."));
+
+        SellerResponse response = new SellerResponse(foundSeller.getId(), foundSeller.getName(), foundSeller.getCreatedAt(), foundSeller.getBio());
+        return Response.ok(response).build();
+    }
+
 
 
 }
