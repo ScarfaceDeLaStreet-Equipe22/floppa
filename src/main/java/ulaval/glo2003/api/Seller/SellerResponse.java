fc599cb7b46ee1e103d84113383717a24664e4c7
Sandrine Comeau <sandrine.comeau.1@ulaval.ca>
2023-02-07 15:31:34 -0500

    s:creation de la route obtention seller

---
+++ src/main/java/ulaval/glo2003/api/Seller/SellerResponse.java
@@ -0,0 +1,53 @@
+package ulaval.glo2003.api.Seller;
+
+import ulaval.glo2003.Domain.Product;
+import ulaval.glo2003.Domain.SellerClasses.DateTime;
+import ulaval.glo2003.api.Product.ProductResponse;
+
+import java.time.Instant;
+import java.util.ArrayList;
+
+public class SellerResponse {
+    public String name;
+    public String bio;
+    public String birthDate;
+    public String email;
+    public String phoneNumber;
+
+    public String createdAt;
+    public ArrayList<ProductResponse> products ;
+    public String id ;
+
+
+
+    public SellerResponse(
+            String id,
+            String name,
+            String bio,
+            String birthDate,
+            String email,
+            String phoneNumber,
+            ArrayList<Product> products, DateTime createdAt) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+        this.email = email;
+        this.phoneNumber = phoneNumber ;
+        this.bio = bio ;
+        this.products = transformProductIntoProductReponse(products) ;
+        this.createdAt = createdAt.getDateTime() ;
+    }
+
+
+
+    private ArrayList<ProductResponse> transformProductIntoProductReponse(ArrayList<Product> productList){
+        ArrayList<ProductResponse> productResponses = new ArrayList<>() ;
+        for(Product product: productList){
+            ProductResponse productResponse = new ProductResponse(product.getTitle(), product.getDescription(), product.getCategory(),
+                    product.getSuggestedPrice(), product.getId(), product.getCreatedAt()) ;
+                    productResponses.add(productResponse) ;
+        }
+
+        return productResponses ;
+    }
+}
