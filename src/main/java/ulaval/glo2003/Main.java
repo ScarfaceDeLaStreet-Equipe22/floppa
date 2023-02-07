48cbd486f18caa01158d8901dec4db21701af1f5
Sandrine Comeau <sandrine.comeau.1@ulaval.ca>
2023-02-07 15:35:47 -0500

    exception vendeur

--- src/main/java/ulaval/glo2003/Main.java
+++ src/main/java/ulaval/glo2003/Main.java
@@ -2,20 +2,39 @@ package ulaval.glo2003;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+import ulaval.glo2003.Domain.Seller;
+import ulaval.glo2003.api.Utils.InvalidParamExceptionMapper;
+import ulaval.glo2003.api.Utils.ItemNotFoundExceptionMapper;
+import ulaval.glo2003.api.Utils.MissingParamExceptionMapper;
+import ulaval.glo2003.health.HealthResource;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        ResourceConfig resourceConfig = new ResourceConfig();
+
+        ArrayList<Seller> sellers = new ArrayList<>();
+
+        // seller and product config
+        SellerRessource seller = new SellerRessource(sellers);
+        ProductRessource produit = new ProductRessource(sellers);
+
+        ResourceConfig resourceConfig = new ResourceConfig().register(new HealthResource());
+
         URI uri = URI.create("http://localhost:8080/");
+        resourceConfig
+                .register(seller)
+                .register(produit)
+                .register(new MissingParamExceptionMapper())
+                .register(new InvalidParamExceptionMapper())
+                .register(new ItemNotFoundExceptionMapper());
 
-        HealthResource healthResource = new HealthResource();
-
-        resourceConfig.register(healthResource);
         HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, resourceConfig);
+
         server.start();
+        System.out.println("salut");
     }
 }
