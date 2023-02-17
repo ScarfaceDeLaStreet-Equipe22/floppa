21412268972cff52fb0630a357f74f08d876ef43
Nabil <nabil.ouchaou.1@ulaval.ca>
2023-02-17 18:49:32 -0500

    début des tests

---
+++ src/test/java/ulaval/glo2003/domain/ProductTest.java
@@ -0,0 +1,14 @@
+package ulaval.glo2003.domain;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+
+    @Test
+    public void testConstructeur(){
+
+    }
+
+}
