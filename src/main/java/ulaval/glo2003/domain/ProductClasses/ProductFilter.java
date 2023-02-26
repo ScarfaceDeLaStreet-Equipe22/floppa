eaa14dc892ab250c255232744fffd47b9aa21ea6
Subdevell <subdevell@gmail.com>
2023-02-26 01:14:30 -0500

    Added min and max price filter

--- src/main/java/ulaval/glo2003/domain/ProductClasses/ProductFilter.java
+++ src/main/java/ulaval/glo2003/domain/ProductClasses/ProductFilter.java
@@ -11,15 +11,15 @@ public class ProductFilter
     private String sellerId;
     private String title;
     private String categoryName;
-    private String minPrice;
-    private String maxPrice;
+    private Amount minPrice;
+    private Amount maxPrice;
 
     public ProductFilter(String sellerId, String title, String categoryName, String minPrice, String maxPrice) {
         this.sellerId = sellerId;
         this.title = title;
         this.categoryName = categoryName;
-        this.minPrice = minPrice;
-        this.maxPrice = maxPrice;
+        this.minPrice = minPrice == null ? null : new Amount(minPrice);
+        this.maxPrice = maxPrice == null ? null : new Amount(maxPrice);
     }
 
     public boolean checkProduct(Product product)
@@ -31,6 +31,12 @@ public class ProductFilter
             return false;
 
         if(!checkProductCategory(product))
+            return false;
+
+        if(!checkProductMinPrice(product))
+            return false;
+
+        if(!checkProductMaxPrice(product))
             return false;
 
         return true;
@@ -58,5 +64,21 @@ public class ProductFilter
             return true;
 
         return product.getCategory().equals(categoryName);
+    }
+
+    private boolean checkProductMinPrice(Product product)
+    {
+        if(minPrice == null)
+            return true;
+
+        return product.suggestedPrice.getAmount() >= minPrice.getAmount();
+    }
+
+    private boolean checkProductMaxPrice(Product product)
+    {
+        if(maxPrice == null)
+            return true;
+
+        return product.suggestedPrice.getAmount() <= maxPrice.getAmount();
     }
 }
