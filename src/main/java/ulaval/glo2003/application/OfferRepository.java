a4933443d2e6fa0c22dd35110e06eeed5fad527f
yacine Kahlis <yacine.kahlis.1@ulaval.ca>
2023-02-27 19:50:24 -0500

    s: Ajout du squelette de OfferRepository et de SellerRepository

---
+++ src/main/java/ulaval/glo2003/application/OfferRepository.java
@@ -0,0 +1,37 @@
+package ulaval.glo2003.application;
+
+import ulaval.glo2003.domain.Offer;
+
+import java.util.ArrayList;
+
+public class OfferRepository implements IRepository<Offer>{
+    @Override
+    public void save(Offer offer) {
+
+    }
+
+    @Override
+    public void remove(Offer offer) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public Offer findById(String id) {
+        return null;
+    }
+
+    @Override
+    public void update(Offer offer) {
+
+    }
+
+    @Override
+    public ArrayList<Offer> findAll() {
+        return null;
+    }
+}
---
+++ src/main/java/ulaval/glo2003/application/SellerRepository.java
@@ -0,0 +1,37 @@
+package ulaval.glo2003.application;
+
+import ulaval.glo2003.domain.Seller;
+
+import java.util.ArrayList;
+
+public class SellerRepository implements IRepository<Seller>{
+    @Override
+    public void save(Seller seller) {
+        
+    }
+
+    @Override
+    public void remove(Seller seller) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public Seller findById(String id) {
+        return null;
+    }
+
+    @Override
+    public void update(Seller seller) {
+
+    }
+
+    @Override
+    public ArrayList<Seller> findAll() {
+        return null;
+    }
+}
