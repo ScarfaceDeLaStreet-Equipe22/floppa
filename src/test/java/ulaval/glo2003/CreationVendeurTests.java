2fa8e0fc80c232b283beed815ee98f5fb5c07aa0
Alexandre Dicaire <alexandre.dicaire.1@ulaval.ca>
2023-04-04 10:09:30 -0400

    f:Ajout configuration de base pour CreationVendeurTests

--- src/test/java/ulaval/glo2003/CreationVendeurTests.java
+++ src/test/java/ulaval/glo2003/CreationVendeurTests.java
@@ -1,10 +1,31 @@
 package ulaval.glo2003;
 
+import jakarta.ws.rs.core.Application;
 import org.glassfish.jersey.test.JerseyTest;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
 public class CreationVendeurTests extends JerseyTest
 {
 
+    // FOR JUNIT 5
+    @BeforeEach
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @AfterEach
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+    // END FOR
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfigProvider().provide();
+    }
 
 
 }
