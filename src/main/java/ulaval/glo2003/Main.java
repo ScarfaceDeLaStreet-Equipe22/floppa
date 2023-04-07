7633056981c489325840c49e24bfc4f2a850487f
yacine Kahlis <yacine.kahlis.1@ulaval.ca>
2023-04-07 17:11:01 -0400

    f: clean du main

--- src/main/java/ulaval/glo2003/Main.java
+++ src/main/java/ulaval/glo2003/Main.java
@@ -32,9 +32,6 @@ public class Main {
         String MONGO_CLUSTER_LINK = Optional.ofNullable(System.getenv("FLOPPA_MONGO_CLUSTER_URL")).orElse("mongodb+srv://admin:admin@processus.5gawlpu.mongodb.net/?retryWrites=true&w=majority&connectTimeoutMS=10000");
         String MONGO_NAME = Optional.ofNullable(System.getenv("FLOPPA_MONGO_DATABASE")).orElse("Processus");
 
-        System.out.println("FLOPPA_MONGO_CLUSTER_URL: " + MONGO_CLUSTER_LINK);
-        System.out.println("FLOPPA_MONGO_DATABASE: " + MONGO_NAME);
-
         MongoClient client = MongoClients.create(MONGO_CLUSTER_LINK);
         datastore = Morphia.createDatastore(client, MONGO_NAME);
         datastore.getMapper().mapPackage("ulaval.glo2003");
